391a20534609d6dea8979c3feed794a9ea0f72d8
shivank96 <shivankg96@gmail.com>
2020-11-16 14:32:53 +0530

    Update ThisSuper.java
    .

--- ThisSuper.java
+++ ThisSuper.java
@@ -1,6 +1,7 @@
 class sup
 {
 	private int a=10;
+        int b=30;
 	sup(int a)
 	{
 		System.out.println("calling current class member(object) using this keyword "+this.a);
@@ -16,6 +17,8 @@ class sub extends sup
 		System.out.println("We use super keyword to execute the parameterized constructor of parent class");
 		System.out.println("calling current class member(object) using this keyword "+this.b);
 		System.out.println("formal argument value "+b);
+                System.out.println("using super keyword we can also call parent class member(variable)"+super.b);
+		//Here we are accessing the parent class variable in sub class using super keyword
 	}
 }
 public class SuperThis 
